40f49b3eb48cdfbc0b01eb9dd9a87fc88d7b4b93
Andreas Buchholz <andy@undev.de>
2016-04-08 11:34:16 +0200

    nested routes for angular-ui-router

--- configurations/angular-ui-router.js
+++ configurations/angular-ui-router.js
@@ -4,21 +4,21 @@ module.exports = {
             function recursiveSearch(node) {
                 return (node && node.callee) &&
                     (
-                    (node.callee.type === 'MemberExpression' &&
-                    node.callee.object.name === '$stateProvider' &&
-                    node.callee.property.name === 'state' &&
-                    node.arguments.length && [1, 2].indexOf(node.arguments.length) !== -1 &&
-                    node.arguments[node.arguments.length - 1].type === 'ObjectExpression')
-                    ||
-                    recursiveSearch(node.callee.object)
+                        (node.callee.type === 'MemberExpression' &&
+                            node.callee.object.name === '$stateProvider' &&
+                            node.callee.property.name === 'state' &&
+                            node.arguments.length && [1, 2].indexOf(node.arguments.length) !== -1 &&
+                            node.arguments[node.arguments.length - 1].type === 'ObjectExpression') ||
+                        recursiveSearch(node.callee.object)
                     )
             }
 
             return recursiveSearch(node);
         },
         extractRoute: function(node) {
-            return node.arguments[node.arguments.length - 1].properties
-                .filter(function(property) {
+            var stateName = node.arguments[0].value;
+            var stateArgs = node.arguments[node.arguments.length - 1].properties;
+            var url = stateArgs.filter(function(property) {
                     return property.key.name === 'url';
                 })
                 .map(function(property) {
@@ -26,6 +26,11 @@ module.exports = {
                 })
                 .pop();
 
+            if (stateName.indexOf(".") > 0) {
+                url = "/" + stateName.split(".")[0] + url;
+            }
+            
+            return url;
         }
     }
 };
